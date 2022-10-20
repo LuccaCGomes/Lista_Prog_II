12fe6b317bd3edd1664f6ca5d9073c9274f6595b
Ramon-Rabello <ramonrabellorj@gmail.com>
2022-10-20 11:38:59 -0300

    feat (4,5) feita

---
+++ Lista 4/Ex4.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int testa_PA(int n, int* v);
+int main(void){
+    int n=0;
+    printf("Digite o numero de termos (>=3): ");
+    scanf("%d",&n);
+    if (n<=2)
+    {
+        do
+    {
+        printf("Digite um numero valido(>=3)");
+        scanf("%d",&n);
+    } while (n<=2);
+    }
+    
+    
+    
+    int *v;
+    v = (int *) malloc(n * sizeof(int));
+    if(v == NULL) {printf("Nao foi possivel alocar a memoria desejada"); exit(1);}
+    for (int i=0;i<n;i++){
+        printf("numero da posicao %d no vetor: ",i+1);
+        scanf("%d",&v[i]);
+    }
+    testa_PA(n,v);
+
+}
+int testa_PA(int n, int* v){
+    int k=v[1]-v[0];
+    int j=0;
+    for (int i=1;i<n;i++){
+        if((v[i]-v[i-1])!=k){
+            j=j+1;
+        }
+        
+    }
+    if(j!=0){
+        printf("0");
+    }
+    else if(j==0){
+        printf("PA de razao: %d",k);
+    }
+}
---
+++ Lista 4/Ex5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+void calcula_corrida(float dist, float *b1, float *b2);
+int main(void){
+float dist,b1,b2;
+printf("Qual a distancia da corrida em km: ");
+scanf("%f",&dist);
+calcula_corrida(dist,&b1,&b2);
+printf("Na bandeira 1 o preco sera: %.2f, e na 2 sera: %.2f",b1,b2);
+
+
+}
+void calcula_corrida(float dist, float *b1, float *b2){
+    *b1=4.95 +2.5*dist;
+    *b2=4.95 +3*dist;
+
+}
