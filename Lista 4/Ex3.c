efd58b6b5a5e19e486a7d65b67c559b9eda56a7d
hfazollo <hfazollo@gmail.com>
2022-10-20 15:00:02 -0300

    Identação

--- Lista 4/Ex3.c
+++ Lista 4/Ex3.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-int numbers[5];
-int *p;
-int n;
-p = numbers;
-*p = 10;
-p++;
-*p = 20;
-p = &numbers[2];
-*p = 30;
-p = numbers + 3;
-*p = 40;
-p = numbers;
-*(p + 4) = 50;
-for (n = 0; n < 5; n++)
-printf(" %d", numbers[n]);
+int main(void)
+{
+    int numbers[5];
+    int *p;
+    int n;
+    p = numbers;
+    *p = 10;
+    p++;
+    *p = 20;
+    p = &numbers[2];
+    *p = 30;
+    p = numbers + 3;
+    *p = 40;
+    p = numbers;
+    *(p + 4) = 50;
+    for (n = 0; n < 5; n++)
+    printf(" %d", numbers[n]);
+
+    // Foi impresso, na ordem, 10, 20, 30, 40 e 50, pois a cada nova vez em que o ponteiro *p era chamado, antes disso, era modificado o local para onde ele apontava, sempre indo uma posição à frente do vetor.
 }
