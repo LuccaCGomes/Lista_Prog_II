c5612ba2e509df9fec35c391d6f6a2cc07ce66cb
hfazollo <hfazollo@gmail.com>
2022-10-24 11:50:00 -0300

    Exercício 13 resolvido + identação de exercícios

--- Lista 4/Ex11.c
+++ Lista 4/Ex11.c
@@ -1,31 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void){
+int main(void)
+{
     float vet1[20], vet2[20] ,vet3[20];
     char vetchar[20];
-    for(int i = 0; i < 20; i++){
+
+    for(int i = 0; i < 20; i++) {
         printf("Digite um numero: ");
         scanf("%f", &vet1[i]);
+
         printf("Digite uma operacao: ");
         scanf(" %c", &vetchar[i]);
+
         printf("Digite outro numero: ");
         scanf("%f", &vet2[i]);
     }
-    for(int j = 0; j < 20; j++){
-        if(vetchar[j] == '+'){
+
+    for(int j = 0; j < 20; j++) {
+        if(vetchar[j] == '+') {
             vet3[j] = (vet1[j] + vet2[j]);
-        }else if(vetchar[j] == '-'){
+        } else if(vetchar[j] == '-') {
             vet3[j] = (vet1[j] - vet2[j]);
-        }else if(vetchar[j] == '*'){
+        } else if(vetchar[j] == '*') {
             vet3[j] = (vet1[j] * vet2[j]);
-        }else if(vetchar[j] == '/'){
+        } else if(vetchar[j] == '/') {
             vet3[j] = (vet1[j] / vet2[j]);
-        }else{
+        } else {
             printf("Oprecao errada, brr s2");
         }
     }
-    for(int h = 0; h < 20; h++){
+
+    for(int h = 0; h < 20; h++) {
         printf("(%.2f %c %.2f = %.3f)", vet1[h], vetchar[h], vet2[h], vet3[h]);
     }
 }
