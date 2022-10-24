20363b7df4bd0a631ce2b257a7e895043979316f
hfazollo <hfazollo@gmail.com>
2022-10-24 16:59:16 -0300

    Exercícios Resolvidos

---
+++ Lista 5/Ex1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <ctype.h>
+
+int calculaVogais(int *qnt, char *vetor);
+
+int main(void)
+{
+    int vogais;
+    char stringVogais[200];
+
+    printf("Digite o string: ");
+    gets(stringVogais);
+
+    calculaVogais(&vogais, stringVogais);
+    
+    printf("O numero de vogais e %d!\n", vogais);
+}
+
+int calculaVogais(int *qnt, char *vetor) {
+    int aux = 0;
+    for(int i = 0; i < strlen(vetor); i++) {
+        vetor[i] = tolower(vetor[i]);
+        if(vetor[i] == 'a' || vetor[i] == 'e' || vetor[i] == 'i' || vetor[i] == 'o' || vetor[i] == 'u') {
+            aux++;
+        }
+    }
+
+    *qnt = aux;
+}
---
+++ Lista 5/Ex3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <ctype.h>
+
+int analisaOcorrencia(char c, char *vet);
+
+int main(void)
+{
+    char frase[20];
+
+    printf("Digite a frase: ");
+    gets(frase);
+
+    char letra;
+    printf("Digite o caracter que deseja analisar a ocorrencia na frase: ");
+    scanf(" %c", &letra);
+    letra = tolower(letra);
+
+    int incidencia = analisaOcorrencia(letra, frase);
+
+    printf("O caracter %c aparece %d vezes na string!\n", letra, incidencia);
+
+    return 0;
+}
+
+int analisaOcorrencia(char c, char *vet) {
+    int contador;
+    
+    for(int i = 0; strlen(vet); i++) {
+        vet[i] = tolower(vet[i]);
+
+        if(vet[i] == c) {
+            contador++;
+        }
+    }
+
+    return contador;
+}
