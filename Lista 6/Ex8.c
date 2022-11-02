7a6164aeaef484cc40fb21794ebe19ccb57c13b2
Ramon-Rabello <ramonrabellorj@gmail.com>
2022-11-02 19:40:08 -0300

    Finalmente

--- Lista 6/Ex8.c
+++ Lista 6/Ex8.c
@@ -3,47 +3,77 @@
 #include <conio.h>
 #include <time.h>
 
-int repeticao(int *vet, int tam, int *n);
+int *repeticao(int *vet, int tam, int *n);
 
 int main(void) 
 {
     srand(time(NULL));
-    int n, cont = 0, *resposta;
+    int n;
 
     printf("Qual o tamanho do vetor? ");
     scanf("%d", &n);
 
     int *vetor;
+    int *aux;
+    int num=0;
+    int *resultado;
     vetor = (int *) malloc (n * sizeof(int));
     if(vetor == NULL) { printf("Nao foi possivel alocar a memoria desejada"); exit(1); }
+    aux = (int *) malloc (n * sizeof(int));
+    if(aux == NULL) { printf("Nao foi possivel alocar a memoria desejada"); exit(1); }
+    
 
+    printf("Vetor original: \n{ ");
     for(int i = 0; i < n; i++) {
         vetor[i] = rand() % (9 + 1 - 0) + 0;
+        printf ("%d ",vetor[i]);
     }
-
-    resposta = repeticao(vetor, n, &cont);
-
-    printf("Numeros repetidos = { ");
-    for(int i = 0; i < n; i++) {
-        printf("%d ", resposta[i]);
-    }
-    printf("}");
-
-}
-
-int repeticao(int *vet, int tam, int *n) {
-    printf("{");
-    for(int i = 0; i < tam; i++){
-
-        printf("%d ", vet[i]);
-
+    for (int i=0;i<n;i++){
+        aux[i]=vetor[i];
     }
     printf("}\n");
 
-    int aux = 0, *retorno;
+    for (int i = 0; i < n; i++){
+        int soma = 0;
 
-    retorno = (int *) malloc(tam * sizeof(int));
-    if(retorno == NULL) { printf("Nao foi possivel alocar a memoria desejada.\n"); exit(1); }
+        for (int k = 0; k < n; k++){
+            if (aux[i] == aux[k] && aux[k] >= 0){
+                soma++;
+                if (soma >= 2){
+                    num+=1;
+                    for (int l = i + 1; l < n; l++){
+                        if (aux[i] == aux[l])
+                        aux[l] =- 10;
+                    }
+
+                }
+
+            }
+        }
+    }
+
+    int *pont;
+    pont=&num;
+    resultado= repeticao(vetor,n, pont);
+    printf("Vetor com numeros repetidos:  \n{ ");
+    for (int i=0;i<num;i++){
+        printf("%d ", resultado[i]);
+    }
+    printf("}");
+    
+}
+
+int *repeticao(int *vet, int tam, int *n) {
+   
+    
+
+    int m=0;
+    int *vet2;
+    vet2 = (int *) malloc (*n * sizeof(int));
+    if(vet2 == NULL) { printf("Nao foi possivel alocar a memoria desejada"); exit(1); }
+
+
+    int aux = 0;
 
     for (int i = 0; i < tam; i++){
         int soma = 0;
@@ -52,8 +82,11 @@ int repeticao(int *vet, int tam, int *n) {
             if (vet[i] == vet[k] && vet[k] >= 0){
                 soma++;
                 if (soma >= 2){
-                    retorno[i] = vet[i];
-                    (*n)++;
+
+                    
+                    vet2[m]=vet[i];
+                    // printf("|%d ", vet2[m]);
+                    m++;
                     for (int l = i + 1; l < tam; l++){
                         if (vet[i] == vet[l])
                         vet[l] =- 10;
@@ -64,6 +97,6 @@ int repeticao(int *vet, int tam, int *n) {
             }
         }
     }
+    return vet2;
 
-    return retorno;
 }
