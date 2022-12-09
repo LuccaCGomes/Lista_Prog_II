16129aaebfd5e1e2211d6d76ea2b8f077037d298
hfazollo <hfazollo@gmail.com>
2022-12-09 19:57:41 -0300

    Exercicio Ordenação Concluído

--- Lista 8/Ex8.c
+++ Lista 8/Ex8.c
@@ -2,13 +2,14 @@
 #include <stdlib.h>
 #include <time.h>
 
-int *bubbleSort(int n, int *vet);
+int bubbleSort(int n, int *vet);
+int *quickSort(int n, int *vet);
 
 int main(void)
 {
     srand(time(NULL));
 
-    int tam, *vetor, *vetorOrdenado;
+    int tam, *vetor, *vetQuick;
     printf("Qual o tamanho do vetor? ");
     scanf("%d", &tam);
 
@@ -16,7 +17,7 @@ int main(void)
     if(vetor == NULL) { printf("Nao foi possivel alocar a memoria desejada.\n"); exit(1); }
 
     for(int i = 0; i < tam; i++) {
-        vetor[i] = rand() % (100 + 1 - 1) + 1;
+        vetor[i] = rand() % (99 + 1 - 1) + 1;
     }
 
     printf("Vetor desordenado = { ");
@@ -25,19 +26,21 @@ int main(void)
     }
     printf("}\n");
 
-    vetorOrdenado = bubbleSort(tam, vetor);
+    bubbleSort(tam, vetor);
+    vetQuick = quickSort(tam, vetor);
 
-    printf("Vetor Bubble Sort = { ");
+    printf("Vetor Quick Sort = { ");
     for(int i = 0; i < tam; i++) {
-        printf("%d ", vetorOrdenado[i]);
+        printf("%d ", vetQuick[i]);
     }
     printf("}\n");
 }
 
-int *bubbleSort(int n, int *vet) {
-    int *bubble, k = 0;
+int bubbleSort(int n, int *vet) {
+    int k = 0;
+
     while(k < n) {
-        for(int i = 0; i < n; i++ ) {
+        for(int i = 0; i < n; i++) {
             if(vet[i] > vet[i+1]) {
                 int temp = vet[i];
                 vet[i] = vet[i+1];
@@ -47,9 +50,41 @@ int *bubbleSort(int n, int *vet) {
         k++;
     }
 
-    bubble = (int *) malloc(n * sizeof(int));
-    if(bubble = NULL) { printf("Nao foi possivel alocar a memoria desejada.\n"); exit(1); }
-    strcpy(bubble, vet);
+    printf("Vetor Bubble Sort = { ");
+    for(int i = 0; i < n; i++) {
+        printf("%d ", vet[i]);
+    }
+    printf("}\n");
+}
 
-    return bubble;
+int *quickSort(int n, int *vet) {
+    int *quick;
+    quick = (int *) malloc(n * sizeof(int));
+
+    if(n > 1) {
+        int x = vet[0], a = 1, b = n - 1;
+
+        do {
+            while(a < n && vet[a] <= x) a++;
+            while(vet[b] > x) b--;
+            if(a < b) {
+                int temp = vet[a];
+                vet[a] = vet[b];
+                vet[b] = temp;
+                a++; b--;
+            }
+        } while(a <= b);
+
+        vet[0] = vet[b];
+        vet[b] = x;
+
+        quickSort(b, vet);
+        quickSort(n - a, &vet[a]);
+    }
+
+    for(int i = 0; i < n; i++) {
+        quick[i] = vet[i];
+    }
+
+    return quick;
 }
