3135835b1cd5ca4c7d727380b8c701eb1b614ad2
hfazollo <hfazollo@gmail.com>
2022-12-12 15:01:46 -0300

    Ex1 Lista 11

--- Lista 11/Ex1.c
+++ Lista 11/Ex1.c
@@ -7,7 +7,40 @@ struct elemento {
 };
 typedef struct elemento Elemento;
 
+Elemento *filtra(Elemento *lst, float min, float max);
+
 int main(void)
 {
-    
+    int min, max;
+    printf("Digite o valor minimo: ");
+    scanf("%d", &min);
+    printf("Digite o valor maximo: ");
+    scanf("%d", &max);
+
+    Elemento *prox;
+    prox = filtra(prox, min, max);
+}
+
+Elemento *filtra(Elemento *lst, float min, float max) {
+    Elemento *elem, *inicial = lst, *anterior = NULL, *aux = NULL;
+    elem = lst;
+
+    while(elem != NULL) {
+        if(anterior == NULL && (elem->info < min || elem->info > max)) {
+            inicial = elem->prox;
+            aux = elem;
+            elem = elem->prox;
+            free(aux);
+        } else if(anterior != NULL && (elem->info < min || elem->info > max)) {
+            anterior->prox = elem->prox;
+            aux = elem;
+            elem = elem->prox;
+            free(aux);
+        } else {
+            anterior = elem;
+            elem = elem->prox;
+        }
+    }
+
+    return inicial;
 }
